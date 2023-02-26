75199235d2d87bfb065bdc68042a2daca27d08be
Uncle Lin <unclelin2020@gmail.com>
2023-02-26 15:26:54 +0800

    build(color): set color in system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#2B2B2B",
+        secondary: "#626262",
+        gray: { DEFAULT: "#F8F8F8", dark: "#8D8D8D", light: "#FCFCFC" }
+      },
       fontFamily: {
         sans: ['PingFang TC', "Microsoft JhengHei", ...defaultTheme.fontFamily.sans],
         serif: ['Noto Serif TC', ...defaultTheme.fontFamily.serif]
