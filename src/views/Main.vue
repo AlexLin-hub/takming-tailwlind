4bac567cb132ea739824213f7de4a8fdd1bf9de7
Uncle Lin <unclelin2020@gmail.com>
2023-03-12 13:55:32 +0800

    fix(headers): add content-type in headers.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -176,7 +176,7 @@
 <script>
 import Card from "../components/card.vue";
 const { CID, SID } = { CID: "CID", SID: "SID" };
-const headers = { CID, SID };
+const headers = { CID, SID, "Content-Type": "application/json" };
 const apiBaseURL = "https://api-takming.herokuapp.com/api/v1";
 
 export default {
